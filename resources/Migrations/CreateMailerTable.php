3399cb3a64fdd25af6fb7321765438c595166545
King Villanueva <king.villanueva@ballys.com>
2024-09-04 15:46:18 +0800

    [feat] Add migration for mailer config

--- resources/Migrations/CreateMailerTable.php
+++ resources/Migrations/CreateMailerTable.php
@@ -3,17 +3,19 @@ class CreateMailerTable
 {
     public function up(PDO $pdo)
     {
-        $pdo->exec("CREATE TABLE IF NOT EXISTS users (
+        $pdo->exec("CREATE TABLE IF NOT EXISTS dressing_mailer (
             id INT AUTO_INCREMENT PRIMARY KEY,
-            username VARCHAR(255) NOT NULL,
-            email VARCHAR(255) NOT NULL,
-            password VARCHAR(255) NOT NULL,
-            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            host VARCHAR(255),
+            username VARCHAR(255),
+            password VARCHAR(255),
+            encryption VARCHAR(255),
+            port VARCHAR(255),
+            address VARCHAR(255),
         )");
     }
 
     public function down(PDO $pdo)
     {
-        $pdo->exec("DROP TABLE IF EXISTS users");
+        $pdo->exec("DROP TABLE IF EXISTS dressing_mailer");
     }
 }
